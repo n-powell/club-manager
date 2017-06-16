675faf787028eb658d337ba93f726982ee692b39
Nick Powell <nickpowells@gmail.com>
2017-06-16 13:19:57 -0700

    add firebase

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from '../player.model';
 import { Router } from '@angular/router';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css']
+  styleUrls: ['./members.component.css'],
+  providers: [PlayerService]
 })
 export class MembersComponent implements OnInit {
   players: Player[] = [
@@ -18,9 +20,10 @@ export class MembersComponent implements OnInit {
   this.router.navigate(['players', clickedPlayer.name]);
   };
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private playerService: PlayerService) { }
 
   ngOnInit() {
+    // this.players = this.playerService.getPlayers(); 
   }
 
 }
--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Player } from './player.model';
 
-import { Injectable } from '@angular/core';
-
 @Injectable()
 export class PlayerService {
 
   constructor() { }
 
+//   getPlayers() {
+//   return player;
+// }
 }
